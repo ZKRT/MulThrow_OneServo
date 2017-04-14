63ffe73efc712ae06d8c4a92b2e0c4df20f38a2f
HarryZeng <461635872@qq.com>
2017-04-14 18:03:09 +0800

    *modify servo pwm value for new struct harware;

--- ZKRT/throw.h
+++ ZKRT/throw.h
@@ -3,48 +3,19 @@
 
 #include "sys.h"
 
-////第一个
-//#define _1_THROW_UNLOCK 1950
-//#define _1_THROW_LOCK   1500
-////第二个
-//#define _2_THROW_UNLOCK	1000
-//#define _2_THROW_LOCK   1500
-////第三个
-//#define _3_THROW_UNLOCK 2000
-//#define _3_THROW_LOCK   1500
-
-//#define THROW_PWM1(value)       TIM_SetCompare2(TIM2, value)
-//#define THROW_PWM2(value)       TIM_SetCompare3(TIM2, value)
-//#define THROW_PWM3(value)       TIM_SetCompare4(TIM2, value)
-//
-//#define GET_PWM1								TIM_GetCapture2(TIM2)
-//#define GET_PWM2								TIM_GetCapture3(TIM2)
-//#define GET_PWM3								TIM_GetCapture4(TIM2)
-
-/////////////////////////////////////////////modify for 多抛单舵机
-////全关
-//#define _ALL_THROW_LOCK 1290
-////第一个
-//#define _1_THROW_UNLOCK 1420
-//#define _1_THROW_LOCK   _ALL_THROW_LOCK
-////第二个
-//#define _2_THROW_UNLOCK	1530
-//#define _2_THROW_LOCK   _ALL_THROW_LOCK
-////第三个
-//#define _3_THROW_UNLOCK 1600
-//#define _3_THROW_LOCK   _ALL_THROW_LOCK
-
 //根据结构设计和调试经验，unlock值大，lock值小，all_lock<1unlock<2unlock<3unlock
 //全关
-#define _ALL_THROW_LOCK 1330   //值越小，锁的越多
+
+//zkrt_debug 170322
+#define _ALL_THROW_LOCK 970   //值越小，锁的越多     num2  old:1340
 //第一个
-#define _1_THROW_UNLOCK 1505   //差值175
+#define _1_THROW_UNLOCK 1150   //差值275										old:1480
 #define _1_THROW_LOCK   _ALL_THROW_LOCK
 //第二个
-#define _2_THROW_UNLOCK	1625   //差值120
+#define _2_THROW_UNLOCK	1330   //差值												old:1575
 #define _2_THROW_LOCK   _ALL_THROW_LOCK
 //第三个
-#define _3_THROW_UNLOCK 1720   //差值95
+#define _3_THROW_UNLOCK 1510   //差值												old:1665
 #define _3_THROW_LOCK   _ALL_THROW_LOCK
 
 
