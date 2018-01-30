8a7ef70563014932d8e0c0def4901fd1e7ecc54e
yanly <413899391@qq.com>
2018-01-30 16:31:20 +0800

    串口输出引脚做火灾探测器的输入检测：需要两根IO脚，检测IO是否通断。IO1输出低电平，IO2上拉输入，当ON时，IO2被拉低，Off时，IO2上拉。

--- HARDWARE/led.c
+++ HARDWARE/led.c
@@ -23,6 +23,51 @@ void LED_Init(void)
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 	GPIO_SetBits(GPIOA,GPIO_Pin_8);
 	
+	
+//PWM1_T3C2，对应PB5
+//PWM2_T3C1，对应PB4
+//PWM3_T2C1，对应PA15
+//PWM4_T3C4，对应PB1
+  
+	//1输入上拉
+	//2输出低电平
+	//3输入上拉
+	//4输出低电平
+//  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+//  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+//  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+//  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;	
+//  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_4;
+//	GPIO_Init(GPIOB, &GPIO_InitStructure);
+//	GPIO_ResetBits(GPIOB, GPIO_Pin_1|GPIO_Pin_4);
+//	
+//  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+//  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+//  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+//  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+//  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+//	GPIO_Init(GPIOB, &GPIO_InitStructure);
+//  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
+//	GPIO_Init(GPIOA, &GPIO_InitStructure);	
+
+
+	//pa9输入上拉
+	//pa10输出低电平
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;	
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_ResetBits(GPIOA, GPIO_Pin_10);
+	
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);	
 	delay_ms(500);
 }
-
--- USER/main.c
+++ USER/main.c
@@ -24,7 +24,7 @@ void bsp_init(void)
 	SystemInit ();		/*系统初始化*/
 	RCC_Configuration();
 	SysTick_Init();
-	TIM_Init();					//挨个通道初始化
+//	TIM_Init();					//挨个通道初始化
 	LED_Init();					//5V使能，在这之前PWM波已经默认无动作了
 	ADC1_Init();
 	CAN_Mode_Init(CAN_Mode_Normal);//CAN初始化环回模式,波特率1Mbps
@@ -34,6 +34,11 @@ void bsp_init(void)
 
 uint8_t status_throw[8] = {0XAA, 0XBB, 0XCC, 0XDD, 0XEE, 0X06, 0X00, 0X00};
 
+uint8_t fire_warning;
+uint8_t fire_alarm;
+uint8_t fire_status;
+#define setBit(x,b)         (x) |=  (1U<<(b))
+#define clearBit(x,b)		    (x) &= ~(1U<<(b))
 int main()
 {
   bsp_init();
@@ -42,7 +47,9 @@ int main()
 	{
 		sub_throw_zkrt_recv_decode_and_zkrt_encode_ack();
 		KEY_Rock();
-		
+//		fire_warning = GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_5);
+//		fire_alarm = GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_15);
+		fire_warning = GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_9);		
 		if (_100ms_count - TimingDelay >= 100)							//100ms一个时间片
 		{
 			_100ms_count = TimingDelay;
@@ -62,25 +69,41 @@ int main()
 				
 				if (MAVLINK_TX_INIT_VAL - TimingDelay > 5000)		//初始化的5S内不执行发送心跳，以后每次都发送心跳
 				{
-					if (GET_PWM1 == _1_THROW_UNLOCK)
+					if(fire_warning)//normal
 					{
-						throw_ack_flag = 1;  //表示抛投1开
-					}
-					else if (GET_PWM1 == _2_THROW_UNLOCK)
-					{
-						throw_ack_flag = 3; //表示抛投1、2开
-					}
-					else if (GET_PWM1 == _3_THROW_UNLOCK)
-					{
-						throw_ack_flag = 7; //表示抛投1、2、3开
+						clearBit(fire_status, 0);
 					}
 					else
 					{
-						throw_ack_flag = 0;
-						THROW_PWM1(_ALL_THROW_LOCK);
+						setBit(fire_status, 0);
 					}
+					if(fire_alarm)
+					{
+						clearBit(fire_status, 1);
+					}			
+					else
+					{
+						setBit(fire_status, 1);
+					}
+//					if (GET_PWM1 == _1_THROW_UNLOCK)
+//					{
+//						throw_ack_flag = 1;  //表示抛投1开
+//					}
+//					else if (GET_PWM1 == _2_THROW_UNLOCK)
+//					{
+//						throw_ack_flag = 3; //表示抛投1、2开
+//					}
+//					else if (GET_PWM1 == _3_THROW_UNLOCK)
+//					{
+//						throw_ack_flag = 7; //表示抛投1、2、3开
+//					}
+//					else
+//					{
+//						throw_ack_flag = 0;
+//						THROW_PWM1(_ALL_THROW_LOCK);
+//					}
 					
-					status_throw[6] = throw_ack_flag;
+					status_throw[6] = fire_status;
 					status_throw[7]++;
 					if (status_throw[7] == 0XFF)
 					{
